f9006bb5763b8abd77a56b8193da57b00a67abe5
ezzye <ezzy.elliott@gmail.com>
2017-10-29 11:37:06 +0000

    Add online for content script.

--- WebContent/content2.js
+++ WebContent/content2.js
@@ -1,27 +1,62 @@
 /**
  * Created by ellioe03 on 26/10/2017.
  */
-var test, nativeObject,nativeObject2, test, yamlString, yamlString2
-
-test = "myStuff: youBet";
-
-nativeObject = YAML.parse(test);
 
 
-yamlString = YAML.stringify(nativeObject,4);
+/*
+ * 6. Display web page error page
+ * with location of error
+ */
 
-nativeObject2 = YAML.load('/Users/ellioe03/workspace/YAMLView-for-Chrome/spec/fixture/example.yml')
 
-yamlString2 = YAML.stringify(nativeObject2,4);
 
-console.log("I am here you all!");
-
-console.log(yamlString);
-
-console.log(yamlString2);
+/*
+ * 5. Display web page with yaml as html
+ * by adding object with div elements
+ */
 
 
 
 
 
+/*
+ * 2. Extract extracts yaml from text;
+ * then tests text using yaml parser,
+ * if it is yaml save yaml object to global variable,
+ * if not return nothing
+ */
 
+
+
+/*
+ * 4. Process data
+ * Notifies backend to convert yaml object to html
+ */
+
+
+
+
+
+
+/*
+ * 3. Init listens to notifications from background and webworkers
+ * three main notifications:
+ *  onInit -  it processes data to html object by calling process data method;
+ *  onyamlToHTML - displays page with yaml, if no html it displays error;
+ *  ongetError - displays an error.
+ * Then notifies backend that init done
+ */
+
+
+
+
+
+
+
+
+/*
+ * 1. Load content from page
+ * test if yaml, if not do nothing.
+ * extract yaml into yamlobject
+ * then pass this data object to init()
+ */
